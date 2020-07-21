6f95fab22c081e6f0ff709e3fad14a80aa7b0f88
voinik <victor_panteleev@hotmail.com>
2020-07-21 17:20:38 +0200

    Add browser history

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const browserHistory = createBrowserHistory();
+
 ReactDOM.render(
-  <Router>
+  <Router history={browserHistory}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
