7f450d4ff4762ad79db2bd04790743ec51b6513e
Ben <Ben@MacBook-Air-van-Ben.local>
2020-09-27 09:32:38 +0200

    feat: working on png-s

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -5,32 +5,22 @@ import { Navbar, Nav, NavItem, NavLink } from 'reactstrap';
 export function Menu() {
 
     return (
-        <div className="menu-container">
-            <Navbar color="faded" light>
-                <Nav navbar>
-                    <NavItem>
-                        <div className="border-bottom link-item">
-                            <NavLink>
-                                <Link to="/">Home</Link>
-                            </NavLink>
-                        </div>
-                    </NavItem>
-                    <NavItem>
-                        <div className="border-bottom link-item">
-                            <NavLink>
-                                <Link to="/about/">About</Link>
-                            </NavLink>
-                        </div>
-                    </NavItem>
-                    <NavItem>
-                        <div className="link-item">
-                            <NavLink>
-                                <Link to="/gallery/">Gallery</Link>
-                            </NavLink>
-                        </div>
-                    </NavItem>
-                </Nav>
-            </Navbar>
-        </div>
+        <>
+            <div className="border-bottom link-item">
+                <NavLink>
+                    <Link to="/">Home</Link>
+                </NavLink>
+            </div>
+            <div className="border-bottom link-item">
+                <NavLink>
+                    <Link to="/about/">About</Link>
+                </NavLink>
+            </div>
+            <div className="link-item">
+                <NavLink>
+                    <Link to="/gallery/">Gallery</Link>
+                </NavLink>
+            </div>
+        </>
     );
 }
